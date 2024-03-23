a36854a168c585abf4f6a4d0f701a782c6990811
Kevin Murray <kevmurray@icloud.com>
2024-03-23 18:09:18 -0500

    Upload mp3s to S3 when done

--- bin/video2audio.bat
+++ bin/video2audio.bat
@@ -3,37 +3,61 @@ echo "WOL, F&F, and CORE Message Video-to-Audio Converter"
 
 REM Get the source file
 SET source=%param%
-IF "%~1" == "" set /P source=Enter video file to convert: 
-IF x%source%==x (
+IF "%~1" == "" set /P source=Enter video file to convert:
+SET source=%source:"=%
+
+REM Verify an input file was provided
+IF "%source%" EQU "" (
   ECHO No input file
   pause
-  EXIT /b 1
+  EXIT /B 1
 )
 
 REM Test source file is mp4
-IF x%source:.mp4=%==x%source% (
+IF "%source:.mp4=%" EQU "%source%" (
   ECHO Input file is not .mp4
   pause
-  EXIT /b 1
+  EXIT /B 1
 )
 
 REM Get the target file
 SET target=%source:mp4=mp3%
 SET target=%target:(video)=(audio)%
-REM SET target=%target:(Video)=(Audio)%
+SET target=%target:(Video)=(Audio)%
 
 REM Get the trim length
 SET trim=9.9
-IF NOT x%source: FF =%==x%source% SET trim=9.8
-IF NOT x%source: CORE =%==x%source% SET trim=30.0
+IF "%source: FF =%" NEQ "%source%" SET trim=9.8
+IF "%source: CORE =%" NEQ "%source%" SET trim=30.0
 
-ECHO Converting: %source:C:\Users\WordofLifeMNMedia\Documents\WOLMessages=...%
-ECHO         to: %target:C:\Users\WordofLifeMNMedia\Documents\WOLMessages=...%
-IF %trim%==9.9  ECHO   trimming: 9.9 seconds for Word Of Life Ministries
-IF %trim%==9.8  ECHO   trimming: 9.8 seconds for Faith and Freedom
-IF %trim%==30.0 ECHO   trimming: 30.0 seconds for C.O.R.E
+ECHO Converting: %source:C:\Users\WordofLifeMNMedia\Documents\WOLMessages\=%
+ECHO         to: %target:C:\Users\WordofLifeMNMedia\Documents\WOLMessages\=%
+ECHO   trimming: %trim% seconds
+ECHO: 
 
-REM ECHO ffmpeg -hide_banner -loglevel warning -stats -i %source% -ss %trim% %target%
-"C:\Program Files\ffmpeg\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe" -hide_banner -loglevel warning -stats -i %source% -ss %trim% %target%
+REM ECHO ffmpeg -hide_banner -loglevel warning -stats -i "%source%"" -ss %trim% "%target%""
+"C:\Program Files\ffmpeg\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe" -hide_banner -loglevel warning -stats -i "%source%" -ss %trim% "%target%"
+
+ECHO:
+REM Get the s3 target
+FOR %%i IN ("%target%") DO (SET s3name=%%~nxi)
+SET s3target=s3://wordoflife.mn.audio/%s3name:~0,4%/%s3name%
+SET s3url=https://s3.us-west-2.amazonaws.com/wordoflife.mn.audio/%s3name:~0,4%/%s3name: =+%
+
+REM Prompt for upload
+ECHO Uploading: %target:C:\Users\WordofLifeMNMedia\Documents\WOLMessages\=%
+ECHO        to: %s3target%
+set /P isUpload="  Confirm: [Y/n]? "
+IF /I "%isUpload%" NEQ "y" (
+  ECHO Aborting upload
+  pause
+  EXIT /B 1
+)
+
+ECHO:
+ECHO %s3url%
+ECHO:
+aws s3 cp "%target%" "%s3target%"
 
 pause
+EXIT /B 0
